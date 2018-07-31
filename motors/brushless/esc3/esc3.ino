198058fa9b0f8f992d131620d6aee43de6af17bc
daniel.pal <pdany86@gmail.com>
2018-08-01 00:34:54 +0200

    ESC3 update: sequence stepping modified, separate header file, serial debug

--- motors/brushless/esc3/esc3.ino
+++ motors/brushless/esc3/esc3.ino
@@ -1,123 +1,78 @@
 
-// MYESC2: BRUSHLESS MOTOR CONTROLLER WITH L293D
+// MYESC3: BRUSHLESS MOTOR CONTROLLER WITH L293D
+
+#include "esc3_base.h"
 
 #define NANO_BOARD_LED 13
 
-#define PIN_ESC_SENSE0 2
-#define PIN_ESC_SENSE1 3
-
-#define PIN_ESC_EN 10
-
-#define PIN_ESC_AEN 4
-#define PIN_ESC_ADIR 5
-#define PIN_ESC_BEN 6
-#define PIN_ESC_BDIR 7
-#define PIN_ESC_CEN 8
-#define PIN_ESC_CDIR 9
-// DIR: 1: SOURCE, 0: DRAIN
-//#define ESC_STATE_PORT_MASK (~((1<<PIN_ESC_AH) | (1<<PIN_ESC_AL) | (1<<PIN_ESC_BH) | (1<<PIN_ESC_BL) | (1<<PIN_ESC_CH) | (1<<PIN_ESC_CL) ))
-
-// SEQUENCE: [[H,L,Z],[H,L,Z],...]
-unsigned char ESC_SEQUENCE[] = {
-  PIN_ESC_ADIR, PIN_ESC_BDIR, PIN_ESC_CEN,
-  PIN_ESC_ADIR, PIN_ESC_CDIR, PIN_ESC_BEN,
-  PIN_ESC_BDIR, PIN_ESC_CDIR, PIN_ESC_AEN,
-  PIN_ESC_BDIR, PIN_ESC_ADIR, PIN_ESC_CEN,
-  PIN_ESC_CDIR, PIN_ESC_ADIR, PIN_ESC_BEN,
-  PIN_ESC_CDIR, PIN_ESC_BDIR, PIN_ESC_AEN
-};
-
-void sense0_isr();
-void esc_step(bool dir = true);
-
-void setup_esc()
-{
-  pinMode(PIN_ESC_EN, OUTPUT);
-
-  pinMode(PIN_ESC_AEN, OUTPUT);
-  pinMode(PIN_ESC_ADIR, OUTPUT);
-  pinMode(PIN_ESC_BEN, OUTPUT);
-  pinMode(PIN_ESC_BDIR, OUTPUT);
-  pinMode(PIN_ESC_CEN, OUTPUT);
-  pinMode(PIN_ESC_CDIR, OUTPUT);
-  
-  pinMode(PIN_ESC_SENSE0, INPUT);
-  pinMode(PIN_ESC_SENSE1, INPUT);
-  attachInterrupt(digitalPinToInterrupt(PIN_ESC_SENSE0), sense0_isr, FALLING);
-
-  esc_step();
-}
-
-volatile char esc_state = 0;
-
-void esc_step(bool dir = true) {
-  if (dir) {
-    esc_state++;
-    if (esc_state > 5) esc_state = 0;
-  } else {
-    if (esc_state <= 0) esc_state = 5;
-    else esc_state--;
-  }
-
-  int pinH = ESC_SEQUENCE[esc_state * 3];
-  int pinL = ESC_SEQUENCE[(esc_state * 3) + 1];
-  int pinZ = ESC_SEQUENCE[(esc_state * 3) + 2];
-
-  // switch FLOATING wire
-  digitalWrite(pinZ, LOW);
-
-  // switch 
-  digitalWrite(pinH, HIGH); // SOURCE
-  digitalWrite(pinL, LOW); // DRAIN
-
-  // activate NON-FLOATING wire(s)
-  digitalWrite(PIN_ESC_AEN, pinZ!=PIN_ESC_AEN);
-  digitalWrite(PIN_ESC_BEN, pinZ!=PIN_ESC_BEN);
-  digitalWrite(PIN_ESC_CEN, pinZ!=PIN_ESC_CEN);
-}
-
 
 bool led_state = false;
-
-volatile long long last_zero_cross_us = 0;
-volatile long long last_dt_us = 0; // time (us) per ESC state
-
-volatile long long wait_dt_us = 0;
-volatile bool switch_pending = false;
-
-volatile int stepCounter = 0;
-
-void sense0_isr() {
-  unsigned long long now = micros();
-  long long dt_us = now - last_zero_cross_us;
-  if(dt_us < 50) return; // debounce(?)
-  
-  last_dt_us = dt_us;
-  //wait_dt_us = last_dt_us/2;
-  wait_dt_us = 1;
-  last_zero_cross_us = now;
-  switch_pending = true;
-
-// -----
-  esc_step();
-  stepCounter++;
-  if(0==(stepCounter%240)) {toggleLed();}
-}
-
-void setup() {
-  pinMode(NANO_BOARD_LED, OUTPUT);
-  setup_esc();
-}
+void toggleLed();
 
 void toggleLed() {
   led_state = !led_state;
   digitalWrite(NANO_BOARD_LED, led_state?(HIGH):(LOW));
 }
 
-int loopCounter = 0;
+volatile long long last_zero_cross_us = 0;
+volatile long long last_dt_us = 0; // time (us) per ESC state
 
+volatile long long wait_dt_us = 0;
+volatile bool switch_pending = false;
+volatile bool sense0_isr_pending = false;
+volatile bool sense0_isr_flag = false;
+
+volatile int stepCounter = 0;
+
+void sense0_isr() {
+  //toggleLed();
+  //if(sense0_isr_pending) return;
+  //sense0_isr_pending = true;
+  sense0_isr_flag = true;
+  // -----
+  esc_step();
+  stepCounter++;
+
+  unsigned long long now = micros();
+  long long dt_us = now - last_zero_cross_us;
+  //if(dt_us < 50) return; // debounce(?)
+  
+  last_dt_us = dt_us;
+  //wait_dt_us = last_dt_us/2;
+  //wait_dt_us = 1;
+  //switch_pending = true;
+  last_zero_cross_us = now;
+
+  //sense0_isr_pending = false;
+}
+bool en_serial = true;
+
+void setup() {
+  pinMode(NANO_BOARD_LED, OUTPUT);
+  if(en_serial) Serial.begin(9600);
+  setup_esc();
+}
+
+
+
+void print_info() {
+  if(!en_serial) return;
+  const int CYCLE_PER_REV = 4;
+  long long US_PER_REV = last_dt_us*6*CYCLE_PER_REV;
+  //long long MS_PER_REV = US_PER_REV/1000;
+  Serial.println((unsigned int)US_PER_REV);
+}
 
 void loop() {
+
+  if(sense0_isr_flag) {
+    sense0_isr_flag = false;
+    if(0==(stepCounter%240)) {
+      toggleLed();
+      print_info();
+    }
+  }
+  
   /*if(switch_pending) {
     unsigned long long now = micros();
     unsigned long long dt_us = (now - last_zero_cross_us);
---
+++ motors/brushless/esc3/esc3_base.h
@@ -0,0 +1,92 @@
+
+
+
+#define PIN_ESC_SENSE0 2
+#define PIN_ESC_SENSE1 3
+
+#define PIN_ESC_EN 10
+
+#define PIN_ESC_AEN 4
+#define PIN_ESC_ADIR 5
+#define PIN_ESC_BEN 6
+#define PIN_ESC_BDIR 7
+#define PIN_ESC_CEN 8
+#define PIN_ESC_CDIR 9
+// DIR: 1: SOURCE, 0: DRAIN
+
+unsigned char ESC_SEQUENCE_H[] = {
+  PIN_ESC_ADIR,
+  PIN_ESC_ADIR,
+  PIN_ESC_BDIR,
+  PIN_ESC_BDIR,
+  PIN_ESC_CDIR,
+  PIN_ESC_CDIR
+};
+
+unsigned char ESC_SEQUENCE_L[] = {
+  PIN_ESC_BDIR,
+  PIN_ESC_CDIR,
+  PIN_ESC_CDIR,
+  PIN_ESC_ADIR,
+  PIN_ESC_ADIR,
+  PIN_ESC_BDIR
+};
+
+unsigned char ESC_SEQUENCE_Z[] = {
+  PIN_ESC_CEN,
+  PIN_ESC_BEN,
+  PIN_ESC_AEN,
+  PIN_ESC_CEN,
+  PIN_ESC_BEN,
+  PIN_ESC_AEN
+};
+
+void sense0_isr();
+void esc_step(bool dir = true);
+
+void setup_esc()
+{
+  pinMode(PIN_ESC_EN, OUTPUT);
+
+  pinMode(PIN_ESC_AEN, OUTPUT);
+  pinMode(PIN_ESC_ADIR, OUTPUT);
+  pinMode(PIN_ESC_BEN, OUTPUT);
+  pinMode(PIN_ESC_BDIR, OUTPUT);
+  pinMode(PIN_ESC_CEN, OUTPUT);
+  pinMode(PIN_ESC_CDIR, OUTPUT);
+  
+  pinMode(PIN_ESC_SENSE0, INPUT);
+  pinMode(PIN_ESC_SENSE1, INPUT);
+  //attachInterrupt(digitalPinToInterrupt(PIN_ESC_SENSE0), sense0_isr, FALLING);
+  attachInterrupt(digitalPinToInterrupt(PIN_ESC_SENSE0), sense0_isr, CHANGE);
+
+  esc_step();
+}
+
+volatile char esc_state = 0;
+
+void esc_step(bool dir = true) {
+  if (dir) {
+    esc_state++;
+    if (esc_state > 5) esc_state = 0;
+  } else {
+    if (esc_state <= 0) esc_state = 5;
+    else esc_state--;
+  }
+  
+  unsigned char pinH = ESC_SEQUENCE_H[esc_state];
+  unsigned char pinL = ESC_SEQUENCE_L[esc_state];
+  unsigned char pinZ = ESC_SEQUENCE_Z[esc_state];
+
+// switch 
+  digitalWrite(pinH, HIGH); // SOURCE
+  digitalWrite(pinL, LOW); // DRAIN
+
+  // switch FLOATING wire
+  //digitalWrite(pinZ, LOW);
+
+  // activate NON-FLOATING wire(s)
+  digitalWrite(PIN_ESC_AEN, pinZ!=PIN_ESC_AEN);
+  digitalWrite(PIN_ESC_BEN, pinZ!=PIN_ESC_BEN);
+  digitalWrite(PIN_ESC_CEN, pinZ!=PIN_ESC_CEN);
+}
