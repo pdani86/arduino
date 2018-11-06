dae88484ebb90dc48a0679e95525c5ecdaa25355
daniel.pal <pdany86@gmail.com>
2018-11-06 21:06:11 +0100

    current limiter project added

---
+++ other/current_limiter/current_limiter.ino
@@ -0,0 +1,24 @@
+#include "current_sensor.h"
+
+CurrentSensor currentSensor(A6);
+
+void setup() {
+  //pinMode(A6,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  currentSensor.updateValue();
+
+  float v = currentSensor.getVoltage();
+  float current = currentSensor.getCurrent();
+  int shuntVal = currentSensor.val();
+  Serial.print("Shunt Val: ");
+  Serial.print(shuntVal);
+  Serial.print(" Shunt Voltage: ");
+  Serial.print(v);
+  Serial.print(" Current: ");
+  Serial.print(current);
+  Serial.println(" A");
+  delay(500);
+}
---
+++ other/current_limiter/current_sensor.h
@@ -0,0 +1,55 @@
+
+class VoltageSensor {
+  const int ANALOG_MAX = 1023;
+  int _pin;
+  int v_ref; // mV
+  int value;
+
+public:
+  inline VoltageSensor(int pin = A6);
+  inline int vRef() {return v_ref;}
+  inline void vRef(int _v_ref) {v_ref = _v_ref;}
+
+  int updateValue();
+  inline int val() {return value;}
+
+  inline float getVoltage();
+};
+
+float VoltageSensor::getVoltage() {
+  float voltage = v_ref*((long long)value)/(float)ANALOG_MAX;
+  return voltage/1000.0f;
+}
+
+VoltageSensor::VoltageSensor(int pin):ANALOG_MAX(1023) {
+  _pin = pin;
+  value = 0;
+  v_ref = 5000; // mV
+  pinMode(_pin,INPUT);
+}
+
+int VoltageSensor::updateValue() {
+   return (value = analogRead(A6));
+}
+
+class CurrentSensor:public VoltageSensor {
+  int r_shunt; // mOhm (resistance to ground)
+
+public:
+  
+  inline CurrentSensor(int pin = A6);
+  
+  inline int rShunt() {return r_shunt;}
+  inline void rShunt(int _r_shunt) {r_shunt = _r_shunt;}
+
+  inline float getCurrent() {
+    float v_shunt = getVoltage();
+    float current = v_shunt/r_shunt*1000.0f;
+    return current;
+  }
+};
+
+CurrentSensor::CurrentSensor(int pin) {
+  r_shunt = 174; // mOhm
+}
+
