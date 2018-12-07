be98c602eb72465d03567fac7f4a67d373654f5a
daniel.pal <pdany86@gmail.com>
2018-12-07 05:42:02 +0100

    motor control inverse

--- other/robotcar00/robotcar00.ino
+++ other/robotcar00/robotcar00.ino
@@ -4,13 +4,15 @@ class MyMotor
   byte enPin;
   byte in1Pin;
   byte in2Pin;
+  bool _invertDir;
   
 public:
-  inline MyMotor(byte _enPin,byte _in1Pin,byte _in2Pin):enPin(_enPin),in1Pin(_in1Pin),in2Pin(_in2Pin) {}
+  inline MyMotor(byte _enPin,byte _in1Pin,byte _in2Pin):enPin(_enPin),in1Pin(_in1Pin),in2Pin(_in2Pin) {_invertDir=false;}
   inline init() {pinMode(enPin,OUTPUT); pinMode(in1Pin,OUTPUT); pinMode(in2Pin,OUTPUT);}
   void pwm(byte val,bool dir = true);
   void brake(byte val,bool dir = false);
   void enable(bool en);
+  inline void invertDir(bool inv) {_invertDir=inv;};
 };
 
 void MyMotor::enable(bool en) {
@@ -18,6 +20,7 @@ void MyMotor::enable(bool en) {
 }
 
 void MyMotor::pwm(byte val,bool dir) {
+  if(_invertDir) dir = !dir;
   analogWrite(enPin,val);
   digitalWrite(in1Pin,dir);
   digitalWrite(in2Pin, (0==val)?dir:(!dir) );
@@ -31,13 +34,13 @@ void MyMotor::brake(byte val,bool dir) {
 
 #define REAR_IR_PIN 4
 
-#define EN_RIGHT_SIDE_PIN A0
-#define PWM_RIGHT_SIDE_1_PIN 5
-#define PWM_RIGHT_SIDE_2_PIN 6
+#define EN_RIGHT_SIDE_PIN 5
+#define PWM_RIGHT_SIDE_1_PIN A0
+#define PWM_RIGHT_SIDE_2_PIN A1
 
-#define EN_LEFT_SIDE_PIN A1
-#define PWM_LEFT_SIDE_1_PIN 9
-#define PWM_LEFT_SIDE_2_PIN 10
+#define EN_LEFT_SIDE_PIN 6
+#define PWM_LEFT_SIDE_1_PIN 7
+#define PWM_LEFT_SIDE_2_PIN 8
 
 
 MyMotor motorRight(EN_RIGHT_SIDE_PIN,PWM_RIGHT_SIDE_1_PIN,PWM_RIGHT_SIDE_2_PIN);
@@ -48,6 +51,7 @@ void setup() {
   pinMode(REAR_IR_PIN,INPUT);
   motorLeft.init();
   motorRight.init();
+  motorRight.invertDir(true);
   /*pinMode(EN_RIGHT_SIDE_PIN,OUTPUT);
   pinMode(PWM_RIGHT_SIDE_1_PIN,OUTPUT);
   pinMode(PWM_RIGHT_SIDE_2_PIN,OUTPUT);
@@ -71,10 +75,10 @@ void stopCar() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  bool obstacleRear = digitalRead(REAR_IR_PIN);
+  bool obstacleRear = !digitalRead(REAR_IR_PIN);
   if(obstacleRear) {
-    forward(35);
+    forward(255);
+    delay(50);
   } else {
     stopCar();
     delay(50);
