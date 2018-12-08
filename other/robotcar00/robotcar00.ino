b2b29dc9cb9347ab3d4261cf761b532b9a3f746d
daniel.pal <pdany86@gmail.com>
2018-12-08 04:17:23 +0100

    remote control (IR)

--- other/robotcar00/robotcar00.ino
+++ other/robotcar00/robotcar00.ino
@@ -1,3 +1,9 @@
+#include <IRremote.h>
+
+#define IR_RECV_PIN A2
+
+IRrecv irrecv(IR_RECV_PIN);
+decode_results ir_results;
 
 class MyMotor
 {
@@ -49,11 +55,26 @@ void MyMotor::brake(byte val,bool dir) {
 MyMotor motorRight(EN_RIGHT_SIDE_PIN,PWM_RIGHT_SIDE_1_PIN,PWM_RIGHT_SIDE_2_PIN);
 MyMotor motorLeft(EN_LEFT_SIDE_PIN,PWM_LEFT_SIDE_1_PIN,PWM_LEFT_SIDE_2_PIN);
 
+/*
+class Car
+{
+  MyMotor leftMotor;
+  MyMotor rightMotor
+  public:
+  inline Car(byte leftEnPin,left)
+    :leftMotor(),
+    rightMotor() {
+      }
+  inline void init() {leftMotor.init(); rightMotor.init();}
+};*/
+
 void setup() {
   pinMode(REAR_IR_PIN,INPUT);
+  irrecv.enableIRIn();
   motorLeft.init();
   motorRight.init();
   motorRight.invertDir(true);
+  motorLeft.invertDir(true);
 }
 
 void forward(byte val) {
@@ -61,18 +82,144 @@ void forward(byte val) {
   motorLeft.pwm(val);
 }
 
-void stopCar() {
-  motorRight.brake(255);
-  motorLeft.brake(255);
+void backward(byte val) {
+  motorRight.pwm(val,false);
+  motorLeft.pwm(val,false);
+}
+
+void turn(byte val,bool right) {
+  if(right) {
+    motorRight.pwm(val,false);
+    motorLeft.pwm(val,true);
+  } else {
+    motorRight.pwm(val,true);
+    motorLeft.pwm(val,false);
+  }
+}
+
+void free_run() {
+  motorRight.freeRun();
+  motorLeft.freeRun();
+}
+
+void brake(byte val) {
+  motorRight.brake(val);
+  motorLeft.brake(val);
+}
+
+enum MOTION_STATE
+{
+  FREE_RUN,
+  BRAKE,
+  FORWARD,
+  BACKWARD,
+  TURN_RIGHT,
+  TURN_LEFT
+};
+
+struct SETTINGS
+{
+  byte pwm;
+  byte brakePwm;
+
+  MOTION_STATE motion_state;
+
+  inline SETTINGS() {pwm = 255; brakePwm = 255; motion_state = BRAKE;}
+};
+
+SETTINGS settings;
+
+enum IR_COMMAND {
+  
+  IRC_PWM_MAX,
+  IRC_PWM_INC = 0x00ffa857,
+  IRC_PWM_DEC = 0x00ffe01f,
+  IRC_BRAKE_PWM_MAX,
+
+  IRC_FORWARD = 0x00ff18e7,
+  IRC_BACKWARD = 0x00ff4a65,
+  IRC_TURN_RIGHT = 0x00ff5aa5,
+  IRC_TURN_LEFT = 0x00ff10ef,
+
+  IRC_BRAKE = 0x00ff38c7,
+  
+  IRC_FREE_RUN = 0x00ff6897,
+};
+
+unsigned long last_ir_value = 0;
+unsigned long last_ir_cmd_timems = 0;
+
+void handleIRcommand() {
+  unsigned long val = ir_results.value;
+  if(val==0xffffffff) val = last_ir_value;
+
+  last_ir_cmd_timems = millis();
+  
+  switch(val) {
+    case IRC_FORWARD:
+      settings.motion_state = FORWARD;
+    break;
+    case IRC_BACKWARD:
+      settings.motion_state = BACKWARD;
+    break;
+    case IRC_BRAKE:
+      settings.motion_state = BRAKE;
+    break;
+    case IRC_FREE_RUN:
+      settings.motion_state = FREE_RUN;
+    break;
+    case IRC_TURN_RIGHT:
+      settings.motion_state = TURN_RIGHT;
+    break;
+    case IRC_TURN_LEFT:
+      settings.motion_state = TURN_LEFT;
+    break;
+    case IRC_PWM_INC:
+      settings.pwm = (settings.pwm>=250)?(255):(settings.pwm+5);
+    break;
+    case IRC_PWM_DEC:
+      settings.pwm = (settings.pwm<=5)?(0):(settings.pwm-5);
+    break;
+    
+    /*case 0xffffffff:
+    break;*/
+    default:
+    break;
+  }
+}
+
+bool pollIRremote() {
+  unsigned long irValue = 0;
+  if(irrecv.decode(&ir_results)) {
+    handleIRcommand(); // (ir_results is global variable)
+    irrecv.resume();
+    return true;
+  }
+  return false;
+}
+
+void updateMotorControl() {
+  switch(settings.motion_state) {
+    case FREE_RUN: free_run(); break;
+    case FORWARD: forward(settings.pwm); break;
+    case TURN_RIGHT: turn(settings.pwm,true); break;
+    case TURN_LEFT: turn(settings.pwm,false); break;
+    case BRAKE: brake(settings.brakePwm); break;
+    case BACKWARD: backward(settings.pwm); break;
+  }
 }
 
 void loop() {
+
+  bool cmdReceived = pollIRremote();
+
+  unsigned long long dtms = millis() - last_ir_cmd_timems;
+  if(dtms>5000 || (dtms>500 && settings.motion_state!=FREE_RUN)) settings.motion_state = BRAKE;
   bool obstacleRear = !digitalRead(REAR_IR_PIN);
-  if(obstacleRear) {
-    forward(255);
-    delay(50);
+  if(obstacleRear && settings.motion_state!=FORWARD) {
+    settings.motion_state = BRAKE;
   } else {
-    stopCar();
-    delay(50);
+    
   }
+  updateMotorControl();
 }
