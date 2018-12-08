cc067b95530fac99ac7c6165dbfc87b4f70647d1
daniel.pal <pdany86@gmail.com>
2018-12-08 23:33:36 +0100

    fix (enableIrIn), update

--- other/robotcar00/ir_cmds.h
+++ other/robotcar00/ir_cmds.h
@@ -3,41 +3,69 @@
 #ifndef __IR_CMDS_H__
 #define __IR_CMDS_H__
 
-namespace IR_CMD {
+namespace IR_REMOTE_CODES {
   namespace REMOTE_TYPE_CARMP3 {
+    enum REMOTE_BTN {
+      CHANNEL_MINUS = 0x00ffa25d,
+      CHANNEL = 0x00ff629d,
+      CHANNEL_PLUS = 0x00ffe21d,
+      PREV = 0x00ff22dd,
+      NEXT = 0x00ff02fd,
+      PLAY_PAUSE = 0x00ffc23d,
+      VOLUME_MINUS = 0x00ffe01f,
+      VOLUME_PLUS = 0x00ffa857,
+      EQ = 0x00ff906f,
+      NUM_0 = 0x00ff6897,
+      NUM_1 = 0x00ff30cf,
+      NUM_2 = 0x00ff18e7,
+      NUM_3 = 0x00ff7a85,
+      NUM_4 = 0x00ff10ef,
+      NUM_5 = 0x00ff38c7,
+      NUM_6 = 0x00ff5aa5,
+      NUM_7 = 0x00ff42bd,
+      NUM_8 = 0x00ff4ab5,
+      NUM_9 = 0x00ff52ad
+    };
+  }
+
+  /*
+  namespace REMOTE_TYPE_SAMSUNG {
     namespace IRCMD {
       enum IR_COMMAND {
         PWM_MAX,
-        PWM_INC = 0x00ffa857,
-        PWM_DEC = 0x00ffe01f,
+        PWM_INC = ,
+        PWM_DEC = ,
         BRAKE_PWM_MAX,
-        FORWARD = 0x00ff18e7,
-        BACKWARD = 0x00ff4ab5,
-        TURN_RIGHT = 0x00ff5aa5,
-        TURN_LEFT = 0x00ff10ef,
-        BRAKE = 0x00ff38c7,
-        FREE_RUN = 0x00ff6897,
+        FORWARD = ,
+        BACKWARD = ,
+        TURN_RIGHT = ,
+        TURN_LEFT = ,
+        BRAKE = ,
+        FREE_RUN = ,
       };
     }
+  }*/
+}
+
+namespace IR_CMD {
+  namespace IRCMD {
+    using namespace IR_REMOTE_CODES::REMOTE_TYPE_CARMP3;
+    enum IR_COMMAND {
+      PWM_MAX,
+      PWM_INC = VOLUME_PLUS,
+      PWM_DEC = VOLUME_MINUS,
+      BRAKE_PWM_MAX,
+      FORWARD = NUM_2,
+      BACKWARD = NUM_8,
+      TURN_RIGHT = NUM_6,
+      TURN_LEFT = NUM_4,
+      BRAKE = NUM_5,
+      FREE_RUN = NUM_0,
+      TOGGLE_OBSTACLE_IGNORING = EQ,
+      TOGGLE_SMOOTH_TORQUE = PLAY_PAUSE,
+      TOGGLE_LAMPS = CHANNEL
+    };
   }
-/*
-  namespace REMOTE_TYPE_SAMSUNG {
-      namespace IRCMD {
-        enum IR_COMMAND {
-          PWM_MAX,
-          PWM_INC = ,
-          PWM_DEC = ,
-          BRAKE_PWM_MAX,
-          FORWARD = ,
-          BACKWARD = ,
-          TURN_RIGHT = ,
-          TURN_LEFT = ,
-          BRAKE = ,
-          FREE_RUN = ,
-        };
-      }
-    }*/
-  
 }
 
 #endif
--- other/robotcar00/robotcar00.ino
+++ other/robotcar00/robotcar00.ino
@@ -3,7 +3,7 @@
 #include "ir_cmds.h"
 #include "car.h"
 
-using namespace IR_CMD::REMOTE_TYPE_CARMP3; // constants for IR commands (buttons)
+using namespace IR_CMD; // constants for IR commands (buttons)
 
 #define IR_RECV_PIN A2
 #define REAR_IR_PIN 4
@@ -16,6 +16,8 @@ using namespace IR_CMD::REMOTE_TYPE_CARMP3; // constants for IR commands (button
 #define EN_LEFT_SIDE_PIN 6
 #define PWM_LEFT_SIDE_1_PIN 7
 #define PWM_LEFT_SIDE_2_PIN 8
+
+#define LAMP_PIN 9
 
 IRrecv irrecv(IR_RECV_PIN);
 decode_results ir_results;
@@ -39,10 +41,12 @@ struct SETTINGS
 {
   byte pwm;
   byte brakePwm;
+  bool obstacleIgnoring;
+  bool smoothTorque;
 
   MOTION_STATE motion_state;
 
-  inline SETTINGS() {pwm = 255; brakePwm = 255; motion_state = BRAKE;}
+  inline SETTINGS() {pwm = 255; brakePwm = 255; motion_state = BRAKE; obstacleIgnoring = false; smoothTorque = false;}
 };
 
 SETTINGS settings;
@@ -51,12 +55,16 @@ SETTINGS settings;
 unsigned long last_ir_value = 0;
 unsigned long last_ir_cmd_timems = 0;
 
+int pwmStep = 12;
+
 void handleIRcommand() {
   unsigned long val = ir_results.value;
   if(val==0xffffffff) val = last_ir_value;
   else last_ir_value = val;
 
   last_ir_cmd_timems = millis();
+
+//Serial.println(val,HEX);
   
   switch(val) {
     case IRCMD::FORWARD:
@@ -78,10 +86,19 @@ void handleIRcommand() {
       settings.motion_state = TURN_LEFT;
     break;
     case IRCMD::PWM_INC:
-      settings.pwm = (settings.pwm>=250)?(255):(settings.pwm+5);
+      settings.pwm = (settings.pwm>=255-pwmStep)?(255):(settings.pwm+pwmStep);
     break;
     case IRCMD::PWM_DEC:
-      settings.pwm = (settings.pwm<=5)?(0):(settings.pwm-5);
+      settings.pwm = (settings.pwm<=pwmStep)?(0):(settings.pwm-pwmStep);
+    break;
+    case IRCMD::TOGGLE_OBSTACLE_IGNORING:
+      settings.obstacleIgnoring = !settings.obstacleIgnoring;
+    break;
+    case IRCMD::TOGGLE_SMOOTH_TORQUE:
+     settings.smoothTorque = !settings.smoothTorque;
+    break;
+    case IRCMD::TOGGLE_LAMPS:
+      digitalWrite(LAMP_PIN,!digitalRead(LAMP_PIN));
     break;
     default:
     break;
@@ -89,7 +106,6 @@ void handleIRcommand() {
 }
 
 bool pollIRremote() {
-  unsigned long irValue = 0;
   if(irrecv.decode(&ir_results)) {
     handleIRcommand(); // (ir_results is global variable)
     irrecv.resume();
@@ -97,20 +113,42 @@ bool pollIRremote() {
   }
   return false;
 }
+float _smoothPwm = 0.0;
+unsigned long _lastSmoothTimeMs = 0;
 
 void updateMotorControl() {
+  byte pwm = settings.pwm;
+  /*
+  if(settings.smoothTorque) {
+    unsigned long now = millis();
+    unsigned long dtms = now - _lastSmoothTimeMs;
+    if(dtms!=0) {
+      float diff = pwm - _smoothPwm;
+      _smoothPwm = _smoothPwm + diff*dtms*0.001f;
+      if(_smoothPwm > 255) pwm = 255;
+      else if(_smoothPwm<0) pwm = 0;
+      else pwm = _smoothPwm;
+      _lastSmoothTimeMs = now;
+    } else {
+      pwm = _smoothPwm;
+    }
+  }*/
+  
   switch(settings.motion_state) {
     case FREE_RUN: car.freeRun(); break;
-    case FORWARD: car.forward(settings.pwm); break;
-    case TURN_RIGHT: car.turn(settings.pwm,true); break;
-    case TURN_LEFT: car.turn(settings.pwm,false); break;
+    case FORWARD: car.forward(pwm); break;
+    case TURN_RIGHT: car.turn(pwm,true); break;
+    case TURN_LEFT: car.turn(pwm,false); break;
     case BRAKE: car.brake(settings.brakePwm); break;
-    case BACKWARD: car.backward(settings.pwm); break;
+    case BACKWARD: car.backward(pwm); break;
   }
 }
 
 void setup() {
   pinMode(REAR_IR_PIN,INPUT);
+  pinMode(LAMP_PIN,OUTPUT);
+  irrecv.enableIRIn();
+  //Serial.begin(9600);
   car.init();
 }
 
@@ -119,10 +157,10 @@ void loop() {
   unsigned long long dtms = millis() - last_ir_cmd_timems;
   if(dtms>5000 || (dtms>500 && settings.motion_state!=FREE_RUN)) settings.motion_state = BRAKE;
   bool obstacleRear = !digitalRead(REAR_IR_PIN);
-  if(obstacleRear && settings.motion_state!=FORWARD) {
+  if(!settings.obstacleIgnoring && obstacleRear && settings.motion_state!=FORWARD) {
     settings.motion_state = BRAKE;
   }
   updateMotorControl();
-  delay(10);
+  delay(5);
 }
 
